9f218df735207fcb1dce188a5a2f1e1b1e1bdb65
Dadleno <dlenox2@huskers.unl.edupwd>
2023-08-25 12:57:44 -0500

    testing

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Dade Lenox
+ * Date: 2023-08-22
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv) { 
+
+    printf("Dade Lenox\n");
+    printf("Computer Science!\n");
+    
+}

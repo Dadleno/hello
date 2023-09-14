445b62daf9fb1b2ea289dd1252f635a39999f9ad
Dadleno <dlenox2@huskers.unl.edupwd>
2023-09-14 12:39:39 -0500

    added folder for the fourth hack

---
+++ Hack04/retire.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Dade Lenox (dlenox2@huskers.unl.edu)
+ * Contributor: Jack Fruhling
+ * 
+ * Date: 2023-09-14
+ * 
+ * This program produces an amortization table for a 401(k) account
+*/
+
+int main(int argc, char** argv[]) {
+
+
+
+    return 0;
+}

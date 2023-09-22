b37ecb97605011c1c818b4c9645d6e9405e155e4
Dadleno <dlenox2@huskers.unl.edupwd>
2023-09-22 14:34:38 -0500

    inital addition

---
+++ Exam01/game.c
@@ -0,0 +1,32 @@
+/**
+ * Exam 1 starter code
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    int startingValue;
+
+    printf("Starting Value: %d\n", startingValue);
+
+    //each round the results should be printed:
+    printf("Round %2d: %3d... ", );
+    //and one of the following:
+    //printf("even, player A scores!\n");
+    //printf(" odd, player B scores!\n");
+
+    printf("Game over!\n");
+    printf("Results: \n");
+    printf("Player   Points   Total Score\n");
+    printf("A        %d        %d\n", );
+    printf("B        %d        %d\n", );
+
+    //depending on the outcome one of the following should be printed:
+    printf("A wins!\n");
+    printf("B wins!\n");
+    printf("Tie!\n");
+
+    return 0;
+}
